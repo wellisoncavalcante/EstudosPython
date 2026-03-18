0ca407d888843a73013a27b7bf99fab30251a9ea
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-18 18:10:17 -0300

    exercicio 06

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n = int(input("Digite um número inteiro: "))
+sucessor = n + 1
+antecessor = n - 1
+
+print("Analisando o número {}, seu sucessor é {} e seu antecessor é {}".format(n, sucessor, antecessor))
