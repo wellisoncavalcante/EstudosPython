5b16de21c04890c2970d0c7f34d6ebd488fa4a0c
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-18 18:25:30 -0300

    exercicio 07

--- ex007.py
+++ ex007.py
@@ -2,4 +2,4 @@ n = int(input("Digite um número qualquer: "))
 dobro = n*2
 triplo = n*3
 raiz = n**(1/2)
-print("O número {}, possui o dobro {}, o triplo {} e a raiz quadrada {}".format(n,dobro, triplo, raiz))
+print("O número {}, possui o dobro {}, \nO triplo {} e a raiz quadrada {}".format(n,dobro, triplo, raiz))
