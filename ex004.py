c4c56c25da2cac6540cc6638ec656da14b1c97fe
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-18 17:16:23 -0300

    exercicio 04

---
+++ ex004.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+soma = num1 + num2
+
+print(soma)
