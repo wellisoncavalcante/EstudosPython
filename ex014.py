4cdcf148a020de2ae0346ddd96229bf75687133e
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-19 18:56:41 -0300

    exercicio 14

---
+++ ex014.py
@@ -0,0 +1,3 @@
+salario = float(input("Informe o salário atual do funcionário: "))
+novo = salario * 1.15
+print(f"O salário atual do funcionário é de R${salario}, mas com o reajuste de 15% de aumento, passa a receber R${novo:.2f}")
