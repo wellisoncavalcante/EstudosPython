c64bffc25e377d040cc394613e9a00ad9bbf4f41
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-19 18:56:28 -0300

    exercicio 10

---
+++ ex010.py
@@ -0,0 +1,5 @@
+x = (int(input("Digite um número para ver a tabuada: ")))
+print("===========================================")
+for y in range(1, 11):
+    print(f"{x} x {y} = {x * y}")
+print("===========================================")
