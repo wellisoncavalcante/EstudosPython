e743ce4d3fd45c39e15a36607a1befee81575267
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-23 18:59:34 -0300

    exercício 27

---
+++ ex027.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia o nome de uma pessoa e diga se ela possui "Cavalcante" no sobrenome.
+
+n = input("Digite seu nome e sobrenome: ")
+y = n.upper()
+z = 'CAVALCANTE' in y
+
+print(f'Se true você possui Cavalcante no sobrenome: {z}')
