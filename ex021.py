b8df5a17765230e3a16c099a881eaec703709f0d
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-23 16:59:37 -0300

    exercicio 21

---
+++ ex021.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um ângulo qualquer e mostra na tela o valor do seno, cosseno e tangente desse ângulo
+import math
+
+n = float(input("Digite um valor de um ângulo qualquer: "))
+m = math.radians(n)
+print(f"O seno desse ângulo é de: {math.sin(m)}, o cosseno é de: {math.cos(m)}, a tangente é de: {math.tan(m)}")
