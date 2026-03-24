7675f114e0038820d604b19dfda28b8a45ac0acc
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-24 19:42:15 -0300

    exercício 021

--- ex021.py
+++ ex021.py
@@ -3,4 +3,4 @@ import math
 
 n = float(input("Digite um valor de um ângulo qualquer: "))
 m = math.radians(n)
-print(f"O seno desse ângulo é de: {math.sin(m)}, o cosseno é de: {math.cos(m)}, a tangente é de: {math.tan(m)}")
+print(f"O seno desse ângulo é de: {math.sin(m):.2f}, o cosseno é de: {math.cos(m):.2f}, a tangente é de: {math.tan(m):.2f}")
