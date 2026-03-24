177e63353335d71d7d798ec432a0024b4fa0174a
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-24 16:37:57 -0300

    exercício 32

---
+++ ex032.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia um número inteiro e mostre na tela se ele é impar ou par.
+
+n = int(input("Digite um número inteiro: "))
+if n % 2 == 0:
+    print("O número escolhido é PAR!")
+else:
+    print("O número escolhido é ÍMPAR!")
